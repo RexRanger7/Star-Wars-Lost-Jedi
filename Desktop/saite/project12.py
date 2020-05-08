2792ea67fd38b9bedd5b1273f35ed68baa38c49a
Алексей Ёлчев <aleksejelcev@MacBook-Pro-Aleksej.local>
2020-05-08 14:43:25 +0400

    first commit

---
+++ Desktop/saite/project12.py
@@ -0,0 +1,530 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+@app.route('/championats')
+def championats():
+    return render_template('championats.html', number=2)
+
+
+@app.route('/epl')
+def epl():
+    return render_template('epl.html', number=2)
+
+
+@app.route('/rpl')
+def rpl():
+    return render_template('rpl.html', number=2)
+
+
+@app.route('/laliga')
+def laliga():
+    return render_template('laliga.html', number=2)
+
+
+@app.route('/bundesliga')
+def bundesliga():
+    return render_template('bundesliga.html', number=2)
+
+
+@app.route('/seriea')
+def seriea():
+    return render_template('seriea.html', number=2)
+
+
+@app.route('/liverpool')
+def liverpool():
+    return render_template('liverpool.html', number=2)
+
+
+@app.route('/city')
+def city():
+    return render_template('city.html', number=2)
+
+
+@app.route('/leicester')
+def leicester():
+    return render_template('leicester.html', number=2)
+
+
+@app.route('/chelsea')
+def chelsea():
+    return render_template('chelsea.html', number=2)
+
+
+@app.route('/united')
+def united():
+    return render_template('united.html', number=2)
+
+
+@app.route('/wolves')
+def wolves():
+    return render_template('wolves.html', number=2)
+
+
+@app.route('/shef')
+def shef():
+    return render_template('shef.html', number=2)
+
+
+@app.route('/tth')
+def tth():
+    return render_template('tth.html', number=2)
+
+
+@app.route('/arsenal')
+def arsenal():
+    return render_template('arsenal.html', number=2)
+
+
+@app.route('/bur')
+def bur():
+    return render_template('bur.html', number=2)
+
+
+@app.route('/kp')
+def kp():
+    return render_template('kp.html', number=2)
+
+
+@app.route('/ev')
+def ev():
+    return render_template('ev.html', number=2)
+
+
+@app.route('/new')
+def new():
+    return render_template('new.html', number=2)
+
+
+@app.route('/sout')
+def sout():
+    return render_template('sout.html', number=2)
+
+
+@app.route('/brighton')
+def brighton():
+    return render_template('brighton.html', number=2)
+
+
+@app.route('/west')
+def west():
+    return render_template('west.html', number=2)
+
+
+@app.route('/watford')
+def watford():
+    return render_template('watford.html', number=2)
+
+
+@app.route('/bour')
+def bour():
+    return render_template('bour.html', number=2)
+
+
+@app.route('/villa')
+def villa():
+    return render_template('villa.html', number=2)
+
+
+@app.route('/norwich')
+def norwich():
+    return render_template('norwich.html', number=2)
+
+
+@app.route('/bayern')
+def bayern():
+    return render_template('bayern.html', number=2)
+
+
+@app.route('/dortmund')
+def dortmund():
+    return render_template('dortmund.html', number=2)
+
+
+@app.route('/leipzig')
+def leipzig():
+    return render_template('leipzig.html', number=2)
+
+
+@app.route('/borussia')
+def borussia():
+    return render_template('borussia.html', number=2)
+
+
+@app.route('/bayer')
+def bayer():
+    return render_template('bayer.html', number=2)
+
+
+@app.route('/shalke')
+def shalke():
+    return render_template('shalke.html', number=2)
+
+
+@app.route('/wolf')
+def wolf():
+    return render_template('wolf.html', number=2)
+
+
+@app.route('/frei')
+def frei():
+    return render_template('frei.html', number=2)
+
+
+@app.route('/hoff')
+def hoff():
+    return render_template('hoff.html', number=2)
+
+
+@app.route('/koln')
+def koln():
+    return render_template('koln.html', number=2)
+
+
+@app.route('/union')
+def union():
+    return render_template('union.html', number=2)
+
+
+@app.route('/frankfurt')
+def frankfurt():
+    return render_template('frankfurt.html', number=2)
+
+
+@app.route('/herta')
+def herta():
+    return render_template('herta.html', number=2)
+
+
+@app.route('/augsburg')
+def augsburg():
+    return render_template('augsburg.html', number=2)
+
+
+@app.route('/mainz')
+def mainz():
+    return render_template('mainz.html', number=2)
+
+
+@app.route('/fortuna')
+def fortuna():
+    return render_template('fortuna.html', number=2)
+
+
+@app.route('/werder')
+def werder():
+    return render_template('werder.html', number=2)
+
+
+@app.route('/pader')
+def pader():
+    return render_template('pader.html', number=2)
+
+
+@app.route('/juve')
+def juve():
+    return render_template('juve.html', number=2)
+
+
+@app.route('/lazio')
+def lazio():
+    return render_template('lazio.html', number=2)
+
+
+@app.route('/inter')
+def inter():
+    return render_template('inter.html', number=2)
+
+
+@app.route('/atalanta')
+def atalanta():
+    return render_template('atalanta.html', number=2)
+
+
+@app.route('/fiori')
+def fiori():
+    return render_template('fiori.html', number=2)
+
+
+@app.route('/bresia')
+def bresia():
+    return render_template('bresia.html', number=2)
+
+
+@app.route('/torino')
+def torino():
+    return render_template('torino.html', number=2)
+
+
+@app.route('/bologna')
+def bologna():
+    return render_template('bologna.html', number=2)
+
+
+@app.route('/verona')
+def verona():
+    return render_template('verona.html', number=2)
+
+
+@app.route('/jenoa')
+def jenoa():
+    return render_template('jenoa.html', number=2)
+
+
+@app.route('/cagliari')
+def cagliari():
+    return render_template('cagliari.html', number=2)
+
+
+@app.route('/udinese')
+def udinese():
+    return render_template('udinese.html', number=2)
+
+
+@app.route('/lecce')
+def lecce():
+    return render_template('lecce.html', number=2)
+
+
+@app.route('/milan')
+def milan():
+    return render_template('milan.html', number=2)
+
+
+@app.route('/napoli')
+def napoli():
+    return render_template('napoli.html', number=2)
+
+
+@app.route('/spal')
+def spal():
+    return render_template('spal.html', number=2)
+
+
+@app.route('/sass')
+def sass():
+    return render_template('sass.html', number=2)
+
+
+@app.route('/samp')
+def samp():
+    return render_template('samp.html', number=2)
+
+
+@app.route('/parma')
+def parma():
+    return render_template('parma.html', number=2)
+
+
+@app.route('/roma')
+def roma():
+    return render_template('roma.html', number=2)
+
+
+@app.route('/tula')
+def tula():
+    return render_template('tula.html', number=2)
+
+
+@app.route('/achmat')
+def achmat():
+    return render_template('achmat.html', number=2)
+
+
+@app.route('/dinamo')
+def dinamo():
+    return render_template('dinamo.html', number=2)
+
+
+@app.route('/zenit')
+def zenit():
+    return render_template('zenit.html', number=2)
+
+
+@app.route('/krasnodar')
+def krasnodar():
+    return render_template('krasnodar.html', number=2)
+
+
+@app.route('/samara')
+def samara():
+    return render_template('samara.html', number=2)
+
+
+@app.route('/loko')
+def loko():
+    return render_template('loko.html', number=2)
+
+
+@app.route('/orenburg')
+def orenburg():
+    return render_template('orenburg.html', number=2)
+
+
+@app.route('/rostov')
+def rostov():
+    return render_template('rostov.html', number=2)
+
+
+@app.route('/rubin')
+def rubin():
+    return render_template('rubin.html', number=2)
+
+
+@app.route('/sochi')
+def sochi():
+    return render_template('sochi.html', number=2)
+
+
+@app.route('/spartak')
+def spartak():
+    return render_template('spartak.html', number=2)
+
+
+@app.route('/tambov')
+def tambov():
+    return render_template('tambov.html', number=2)
+
+
+@app.route('/ural')
+def ural():
+    return render_template('ural.html', number=2)
+
+
+@app.route('/ufa')
+def ufa():
+    return render_template('ufa.html', number=2)
+
+
+@app.route('/cska')
+def cska():
+    return render_template('cska.html', number=2)
+
+
+@app.route('/barca')
+def barca():
+    return render_template('barca.html', number=2)
+
+
+@app.route('/alaves')
+def alaves():
+    return render_template('alaves.html', number=2)
+
+
+@app.route('/atletic')
+def atletic():
+    return render_template('atletic.html', number=2)
+
+
+@app.route('/atletico')
+def atletico():
+    return render_template('atletico.html', number=2)
+
+
+@app.route('/espan')
+def espan():
+    return render_template('espan.html', number=2)
+
+
+@app.route('/eibar')
+def eibar():
+    return render_template('eibar.html', number=2)
+
+
+@app.route('/getafe')
+def getafe():
+    return render_template('getafe.html', number=2)
+
+
+@app.route('/celta')
+def celta():
+    return render_template('celta.html', number=2)
+
+
+@app.route('/sevilla')
+def sevilla():
+    return render_template('sevilla.html', number=2)
+
+
+@app.route('/sos')
+def sos():
+    return render_template('sos.html', number=2)
+
+
+@app.route('/real')
+def real():
+    return render_template('real.html', number=2)
+
+
+@app.route('/osasuna')
+def osasuna():
+    return render_template('osasuna.html', number=2)
+
+
+@app.route('/mallorca')
+def mallorca():
+    return render_template('mallorca.html', number=2)
+
+
+@app.route('/leganes')
+def leganes():
+    return render_template('leganes.html', number=2)
+
+
+@app.route('/levante')
+def levante():
+    return render_template('levante.html', number=2)
+
+
+@app.route('/granada')
+def granada():
+    return render_template('granada.html', number=2)
+
+
+@app.route('/betis')
+def betis():
+    return render_template('betis.html', number=2)
+
+
+@app.route('/valencia')
+def valencia():
+    return render_template('valencia.html', number=2)
+
+
+@app.route('/vallad')
+def vallad():
+    return render_template('vallad.html', number=2)
+
+
+@app.route('/vill')
+def vill():
+    return render_template('vill.html', number=2)
+
+
+@app.route('/laligamatches')
+def laligamatches():
+    return render_template('laligamatches.html', number=2)
+
+
+@app.route('/eplmatches')
+def eplmatches():
+    return render_template('eplmatches.html', number=2)
+
+
+@app.route('/serieamatches')
+def serieamatches():
+    return render_template('serieamatches.html', number=2)
+
+
+@app.route('/bundesligamatches')
+def bundesligamatches():
+    return render_template('bundesligamatches.html', number=2)
+
+
+@app.route('/rplmatches')
+def rplmatches():
+    return render_template('rplmatches.html', number=2)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
